a6b470a182e68234c77f63aea297d1635d93016f
ErosZy <395693101@qq.com>
2015-02-26 01:16:15 +0800

    修复removeChild方法

--- src/DisplayObjectContainer.js
+++ src/DisplayObjectContainer.js
@@ -9,35 +9,35 @@ function DisplayObjectContainer(){
     this._childList = [];
 }
 
-DisplayObjectContainer.prototype = {
-    constructor: DisplayObjectContainer,
-    addChild: function(obj){
-        var self = this;
+DisplayObjectContainer.prototype.addChild = function(obj){
+    var self = this;
 
-        if(obj instanceof DisplayObject){
-            self._childList.push(obj);
-            obj.parent = self;
-            obj.objectIndex = self.objectIndex+"."+self._childList.length;
-        }
+    if(obj instanceof DisplayObject){
+        self._childList.push(obj);
+        obj.parent = self;
+        obj.objectIndex = self.objectIndex+"."+self._childList.length;
+    }
 
-        return self;
-    },
-    removeChild: function(obj){
-        var self = this,
-            item;
+    return self;
+};
 
-        if(obj instanceof DisplayObject){
-            for(var i = self._childList.length - 1; i >= 0; i++){
-                item = self._childList[i];
-                if(item.aIndex == obj.aIndex){
-                    arrProto.splice.call(self._childList,i,1);
-                }
+DisplayObjectContainer.prototype.removeChild = function(obj){
+    var self = this,
+        item;
+
+    if(obj instanceof DisplayObject){
+        for(var i = self._childList.length - 1; i >= 0; i--){
+            item = self._childList[i];
+            if(item.aIndex == obj.aIndex){
+                arrProto.splice.call(self._childList,i,1);
             }
         }
+    }
 
-        return self;
-    },
-    getChildAt: function(index){
+    return self;
+};
+
+DisplayObjectContainer.prototype.getChildAt = function(index){
         var self = this,
             len = self._childList.length;
 
@@ -48,15 +48,15 @@ DisplayObjectContainer.prototype = {
         }
 
         return self._childList[index];
-    },
-    contains: function(obj){
-        var self = this;
+};
 
-        if(obj instanceof DisplayObject){
-            return Util.inArray(self._childList,obj,function(obj,item){
-                return obj.aIndex == item.aIndex;
-            }) == -1 ? false : true;
-        }
+DisplayObjectContainer.prototype.contains = function(obj){
+    var self = this;
+
+    if(obj instanceof DisplayObject){
+        return Util.inArray(self._childList,obj,function(obj,item){
+            return obj.aIndex == item.aIndex;
+        }) == -1 ? false : true;
     }
 };
 
