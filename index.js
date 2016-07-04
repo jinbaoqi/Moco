e092602d1a5d8ab201ee8d8195fc67549388f8ff
zyEros <395693101@qq.com>
2016-07-04 21:59:08 +0800

    merge

---
+++ index.js
@@ -0,0 +1,29 @@
+function flattern() {
+
+
+}
+
+var arr = [1, [2, 3], [4, 5, [6, 7]]];
+
+arr = flattern(arr);
+
+//[1,2,3,4,5,6,7]
+
+
+function different() {
+    for(var i=0;i<arr1.length;i++){
+        for(var k=0;k<arr2.length;k++){
+            if(arr1[i]!=arr2[i]){
+
+
+            }
+        }
+    }2312
+}
+
+var arr1 = [1, 2, 3, 4, 5];
+var arr2 = [3, 4, 7, 8];
+
+different(arr1, arr2);
+
+//[7,8]
