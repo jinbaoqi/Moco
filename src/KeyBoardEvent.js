cbf0c17d2733703d4b64e3ef7b07b51dd0d1c782
zyEros <395693101@qq.com>
2016-07-06 00:10:54 +0800

    by zhaoyang

--- src/KeyBoardEvent.js
+++
@@ -1,9 +0,0 @@
-var KeyBoardEvent = {
-    KEY_DOWN: "keydown",
-    KEY_UP: "keyup",
-    KEY_PRESS: "keypress"
-};
-
-KeyBoardEvent.nameList = Util.keys(KeyBoardEvent);
-
-Util.extends(KeyBoardEvent, EventCore);
